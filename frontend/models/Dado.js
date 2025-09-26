47df3084fa98bd6f13fcaab2bc4c01bf39bebcd3
m9844924-alt <m9844924@gmail.com>
2025-09-26 17:01:34 -0500

    feat: punto 3

--- frontend/models/Dado.js
+++ frontend/models/Dado.js
@@ -1,24 +1,21 @@
 export class Dado {
+  static dados = [0, 0];
 
-    //atributos
-    constructor() {
-        this.dados = [0, 0];
-    }
+  // Aqui se definirán metodos relacionados con los dados
+  // Lanzar, verificacion de dobles y sumar
 
-    // Aqui se definirán metodos relacionados con los dados
-    // Lanzar, verificacion de dobles y sumar 
-    lanzar() {
-        this.dados[0] = Math.floor(Math.random() * 6) + 1; // Genera un número aleatorio entre 1 y 6
-        this.dados[1] = Math.floor(Math.random() * 6) + 1;
-        return this.dados;
-    }
+  static lanzar() {
+    this.dados[0] = Math.floor(Math.random() * 6) + 1; // Genera un número aleatorio entre 1 y 6
+    this.dados[1] = Math.floor(Math.random() * 6) + 1;
+    return this;
+  }
 
-    verificarDobles() {  // Si esto es verdad se concede otro turno al jugador
-        return this.dados[0] === this.dados[1];
-    }
+  static verificarDobles() {
+    // Si esto es verdad se concede otro turno al jugador
+    return this.dados[0] === this.dados[1];
+  }
 
-    sumarDados() {
-        return this.dados[0] + this.dados[1];
-    }
-
+  static sumarDados() {
+    return this.dados[0] + this.dados[1];
+  }
 }
